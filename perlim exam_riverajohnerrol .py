844d8a9daabb9f465e572fab0392da4c98c7982c
johnerrol <riverajohnerrol@yahoo.com>
2012-07-16 12:34:01 +0800

    exam

---
+++ perlim exam_riverajohnerrol .py
@@ -0,0 +1,113 @@
+class BasketballPlayer:
+    def SetInfo(self,name,age,PointsLastSeason,Personal Fouls,Injuries,Blood Type,Time Playing,Team,Sex):
+        self.Name=name
+        self.Age=age
+        self.Points Last Season=points last season
+        self.Personal Fouls=personal fouls
+        self.Injuries=injuries
+        self.Blood Type=blood type
+        self.Time Playing=time playing
+        self.Team=team
+        self.Sex=sex
+    def show(self):
+        print("Name:%s" %(self.Name))
+        print("Name:%i" %(self.Age))
+        print("Points Last Season: %s" % (self.Points Last Season))
+        print("Personal Fouls: %s" %(self.Personal Fouls))
+        print("Injuries: %i" %(self.Injuries))
+        print("Blood Type: %s" %(self.Blood Type))
+        print("Time Playing: %i" %(self.Time Playing))
+        print("Team: %s" %(self.Team))
+        print("Sex: %i" %(self.Sex))
+
+class BasketballPlayerInfo(Information):
+    def SetInfo(self,name,age,pointslastseason,personal fouls,injuries,blood type,time playing,time,sex):
+        self.Name=name
+        self.Age=age
+        self.PointsLastSeason=""
+        self.Personal Fouls=6
+        sef.Injuries=injuries
+        self.Blood Type=blood type
+        self.Time Playing=time playing
+        self.Team="L.A. Lakers"
+        self.Sex=Sex
+
+class BasketballPlayerInfo(Information):
+    def SetInfo(self,name,age,pointslastseason,personal fouls,injuries,blool type,time playing,team,sex):
+        self.Name=name
+        self.Age=age
+        self.PointsLastSeason=""
+        self.Personal Fouls=6
+        sef.Injuries=injuries
+        self.Blood Type=blood type
+        self.Time Playing=time playing
+        self.Team="L.A. Lakers"
+        self.Sex=Sex
+
+class BasketballPlayerInfo(Information):
+    def SetInfo(self,name,age,pointslastseason,personal fouls,injuries,blood type,time playing,team,sex):
+        self.Name=name
+        self.Age=age
+        self.PointsLastSeason=""        
+        self.Personal Fouls=6
+        sef.Injuries=injuries
+        self.Blood Type=blood type
+        self.Time Playing=time playing
+        self.Team="L.A. Lakers"
+        self.Sex=Sex
+
+
+ListOfBasketball Player=[]
+basketball player=BasketballPlayerInfo()
+basketball player.SetInfo("Jane",17,)
+ListOfBasketball Players.append(basketball player)
+
+basketball player=BasketballPlayerInfo()
+basketball player.SetInfo("Errol",19,)
+ListOfBasketball players.append(basketball player)
+
+basketball player=BasketballPlayerInfo()
+basketball player.SetInfo("Elvira",22)
+ListOfBasketball Players.append(Basketball Player)
+
+basketball player=BasketballPlayerInfo()
+basketball player.SetInfo("Joy",19)
+ListOfBasketball Players.append(Basketball Player)
+
+basketball player=BasketballPlayerInfo()
+baketball player.SetInfo("BJ",19)
+ListOfBasketball Players.append(Basketball Player)
+
+basketball player=BasketballPlayerInfo()
+basketball.SetInfo("Jamaica",18)
+ListOfBasketball Players.append(Basketball Player)
+
+basketball player=BasketballPlayerInfo()
+basketball player.SetInfo("Lorna",27)
+ListOfBasketball Players.append(Basketball Player)
+
+basketball player=BasketballPlayerInfo()
+basketball player.SetInfo("Ate Ruth",18)
+ListOfBasketball Players.append(Basketball Player)
+
+basketball player=BasketballPlayerInfo()
+basketball player.SetInfo("Geraldine",16)
+ListOfBasketball Players.append(Basketball Player)
+
+basketball player=BasketballPlayerInfo()
+basketball player.SetInfo("Maureen",20)
+ListOfBasketball Players.append(Basketball Player)
+
+student=BasketballPlayerInfo()
+student.SetInfo("Jamie",16)
+ListOfStudents.append(Basketball Player)
+
+
+
+for basketball player in ListOfBasketball Players:
+    basketball player.show()
+    Show Name()
+    Show Team()
+        
+
+    
