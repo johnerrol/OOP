ced6f61add03761d3cae622c1a4e00f213dec160
johnerrol <riverajohnerrol@yahoo.com>
2012-08-08 10:49:55 +0800

    app_size 1 and app_size 2

---
+++ app_size.py
@@ -0,0 +1,18 @@
+import tkinter
+
+
+class Application(tkinter.Frame):
+    def __init__(self,master):
+        tkinter.Frame.__init__(self,master)
+        self.config(width=1000)
+        self.config(height=200)
+        self.config(bg="#F0F0F0")
+        self.pack()
+        self.CreateWidget()
+    def  CreateWidget(self):
+        pass
+
+root=tkinter.Tk()
+app=Application(master=root)
+app.mainloop()
+root.destroy()        
