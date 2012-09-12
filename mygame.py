52fd30a53879f4f85ea58135f2d0d4f452e383a0
johnerrol <riverajohnerrol@yahoo.com>
2012-09-12 11:14:51 +0800

    my game

---
+++ mygame.py
@@ -0,0 +1,27 @@
+Python 3.2.3 (default, Apr 11 2012, 07:15:24) [MSC v.1500 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> ================================ RESTART ================================
+>>> 
+Traceback (most recent call last):
+  File "C:/Python32/pygame.py", line 1, in <module>
+    import pygame
+  File "C:/Python32\pygame.py", line 2, in <module>
+    pygame.init()
+AttributeError: 'module' object has no attribute 'init'
+>>> ================================ RESTART ================================
+>>> 
+Traceback (most recent call last):
+  File "C:/Python32/pygame.py", line 1, in <module>
+    import pygame
+  File "C:/Python32\pygame.py", line 2, in <module>
+    pygame.init()
+AttributeError: 'module' object has no attribute 'init'
+>>> ================================ RESTART ================================
+>>> 
+Traceback (most recent call last):
+  File "C:/Python32/pygame.py", line 1, in <module>
+    import pygame
+  File "C:/Python32\pygame.py", line 2, in <module>
+    pygame.init()
+AttributeError: 'module' object has no attribute 'init'
+>>> ================================ RESTART ================================
